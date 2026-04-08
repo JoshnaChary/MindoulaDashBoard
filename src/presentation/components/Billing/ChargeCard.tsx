264c0e863522817aa31ef57b5791944d6baa1df0
JoshnaChary <106054243+JoshnaChary@users.noreply.github.com>
2026-04-08 06:28:34 +0530

    maintainability issues are resolved.

--- src/presentation/components/Billing/ChargeCard.tsx
+++ src/presentation/components/Billing/ChargeCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { AppText } from '../../../components/atoms/AppText';
 import { Colors } from '../../../core/theme/colors';
 import { Spacing } from '../../../core/theme/spacing';
